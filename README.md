1a537e208f0671c9b8a10bf665c3b846c621f501
tmuife <43266626@qq.com>
2024-12-03 15:58:40 +0800

    support concurrently call text embedding api and batch update the records to DB

--- README.md
+++ README.md
@@ -15,27 +15,48 @@ vim .env
 
 参考如下：
 
-从user开始到increment是数据库信息
+text_embedding_urls： 可以有多个，以逗号分割
+
+query_batch_size：每次从数据库中取的记录数
+
+embedding_batch_size：多少条记录会发送给同一个restapi，因此并发为query_batch_size/embedding_batch_size，如果text_embedding_urls比较少，可能会分到多个需要embedding的batch
+
+vector_column：用来存储向量数据的列，目前从rest api 得到向量数据后强转float32
 
 ```
-user='vector'
-password='<password>'
-dsn='<ip>:1521/vecpdb1'
+user='<DB_USERNAME>'
+password='<DB_PASSWORD>'
+dsn='<IP>:1521/<PDB>'
 min=1
 max=10
 increment=1
 
-text_embedding_url='http://<ip>:8899/chat/text_embedding/'
+text_embedding_url='http://<REST API IP>:8899/chat/text_embedding/'
 
-fetch_table='es_docs_10000'
+#embedding rest api lists, split with comma
+text_embedding_urls='http://<REST API IP>:8899/chat/text_embedding/,http://<REST API IP>:8899/chat/text_embedding/'
+#query from DB, maybe the source data is huge, so you can query it by page
+query_batch_size=1000
+#how many records was sent to one embedding api, so the concurrency is query_batch_size/embedding_batch_size
+embedding_batch_size=100
+fetch_sql='select id,content from es_docs_10000 order by id offset :1 rows fetch next :2 rows only'
+#this column should be primary key or unique
 key_column='id'
+#this column is origin content which want to be embedded
 origin_column='content'
+#this column store the VECTOR data
 vector_column='vec_content'
 update_sql='update es_docs_10000 set vec_content=:1 where id=:2'
 
 ```
 
 # 检查embed.py&运行
+
+embed.py脚本中可能有测试的条件信息及输出信息，请自行检查修改。
+
+对于向量后的数据更新，目前脚本也是以批量方式进行更新，可能会存在更新不成功的情况，目前脚本未对更新不成功的数据进行处理。
+
+**运行前请自行保证数据的备份。**
 
 ```
 python embed.py
--- embed.py
+++ embed.py
@@ -1,6 +1,6 @@
 from decouple import config
 import oracledb
-import datetime
+import datetime as dt
 import requests
 import numpy as np
 import logging
@@ -8,7 +8,8 @@ import array
 import asyncio
 from typing import Any, Union, cast
 logger = logging.getLogger("Oracle Database")
-
+logging.basicConfig(format='%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s',
+                    level=logging.INFO)
 class DB:
 
     def __init__(self, **kwargs):
@@ -77,9 +78,9 @@ class DB:
                 connection.outputtypehandler = self.output_type_handler
                 with connection.cursor() as cursor:
                     if data is None:
-                        await cursor.execute(sql)
+                        await cursor.executemany(sql)
                     else:
-                        await cursor.execute(sql, data)
+                        await cursor.executemany(sql, data)
                     await connection.commit()
                 return True
         except Exception as e:
@@ -108,8 +109,8 @@ class DB:
                     return [dict(zip(columns, row)) for row in rows]
                 else:
                     return []
-async def call_text_embedding(texts: list[dict]):
-    url = config("text_embedding_url")
+async def call_text_embedding(data_batch: dict):
+    url = data_batch.get("api", config("text_embedding_url"))
     # 设置请求头
     headers = {
         'accept': 'application/json',
@@ -117,51 +118,66 @@ async def call_text_embedding(texts: list[dict]):
         "access_token": ""
     }
     result = []
-    for text in texts:
+    records = data_batch["datas"]
+    for record in records:
         try:
-            data = {"text": text[config("origin_column")], "embed_model": "bge_m3"}
+            data = {"text": record[config("origin_column")], "embed_model": "bge_m3"}
             # 发送 GET 请求
             response = requests.post(url, headers=headers, json=data)
             # 输出返回的结果
             if response.status_code == 200:
-                sss = response.json()
                 #logging.info("成功调用接口:", response.json())
                 if isinstance(response.json(), dict):
                     if response.json()["code"] == 200:
-                        #return response.json()["data"]
-                        text[config("vector_column")] = np.array(response.json()["data"].replace("[","").replace("]","").split(",")).astype(np.float64)
-                        result.append(text)
+                        record[config("vector_column")] = np.array(response.json()["data"].replace("[","").replace("]","").split(",")).astype(np.float32)
+                        result.append(record)
             else:
                 logging.error(f"请求失败，状态码: {response.status_code}, 错误信息: {response.json()}")
         except Exception as e:
-            print(e)
-            print(text)
+            logging.error(e)
+            logging.error(record)
     return result
 
 
 async def embedding():
     oracle_db = DB()
-    query_sql = "select %s,%s from %s " % (config("key_column"), config("origin_column"), config("fetch_table"))
-    results: list[dict] = await oracle_db.query(sql=query_sql, data=[])
-    #batches = [
-    #    results[i: i + int(config("batch_size"))]
-    #    for i in range(0, len(results), int(config("batch_size")))
-    #]
-    #embedding_results =  await asyncio.gather(
-    #    *[call_text_embedding(item) for item in batches]
-    #)
-    columns: list = [config("vector_column"), config("key_column")]
-    for item in results:
-        datas = []
-        embedding_result = await call_text_embedding([item])
-        for column in columns:
-            datas.append(embedding_result[0][column])
-        if len(datas) != 2:
-            logging.error("datas length is not suitable")
-        else:
-            r = await oracle_db.execute(sql=config("update_sql"), data=datas)
-            if not r:
-                logging.error("update error ", item)
+    query_sql = config("fetch_sql")
+    update_sql = config("update_sql")
+    query_batch_size = int(config("query_batch_size"))
+    embedding_batch_size = int(config("embedding_batch_size"))
+    rest_api_arr = config("text_embedding_urls").split(",")
+    index = 0
+    while True:
+        start = index*query_batch_size
+        end = start+query_batch_size
+        query_result = await oracle_db.query(sql=query_sql, data=[start, end])
+        logging.info("The %sth query batch was query out and start to call embedding concurrently at %s" % (index+1, dt.datetime.now()))
+        api_with_data_object_list = []
+        batches = [
+           query_result[i: i + embedding_batch_size]
+           for i in range(0, len(query_result), embedding_batch_size)
+        ]
+        for i, batch in enumerate(batches):
+            assigned_rest_api = rest_api_arr[i % len(rest_api_arr)]  # 根据索引进行轮询分配
+            api_with_data_object_list.append({"api": assigned_rest_api, "datas": batch})
+
+        embedding_results = await asyncio.gather(
+           *[call_text_embedding(item) for item in api_with_data_object_list]
+        )
+        logging.info("The %sth query batch embedding finish at %s" % (index + 1, dt.datetime.now()))
+
+        data_params = []
+        for _embeds in embedding_results:
+            for _embed in _embeds:
+                data_params.append([_embed[config("vector_column")], _embed[config("key_column")]])
+        insert_result = await oracle_db.execute(sql=update_sql, data=data_params)
+        if not insert_result:
+            logging.error("update error batch data", embedding_results)
+        logging.info("The %sth query batch updated finish at %s" % (index + 1, dt.datetime.now()))
+        index += 1
+        if len(query_result) < query_batch_size:
+            break
+
 
 if __name__ == "__main__":
     asyncio.run(embedding())
